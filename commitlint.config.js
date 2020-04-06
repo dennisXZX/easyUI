294c8632d8051ffd587faa03fc9d8f2d2d1e5998
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-04-06 15:34:09 +0800

    chore: add commitlint, lint-staged and prettier configs

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fixing
+        "docs", // Add or update documentation
+        "style", // Add or update style, which should not affect any logic
+        "refactor", // Refactor (Not adding new feature or fixing bugs)
+        "test", // Add or update tests
+        "revert", // Revert
+        "chore", // Other changes
+      ],
+    ],
+    "type-empty": [2, "never"], // Warning for inappropriate types
+    "subject-empty": [2, "never"], // Warning for empty subject
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+  },
+};
