11c5073711461c52fe26d159724c20cc2b8d9bb9
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-04-11 09:54:58 +0800

    feat: add Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
 import React from 'react';
-import Menu, { MenuMode } from './components/Menu/menu';
-import MenuItem from './components/Menu/menu-item';
-import SubMenu from './components/Menu/sub-menu';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import Icon from './components/Icon/icon';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 function App() {
 	return (
 		<div className="App">
-			<Menu
-				onSelect={index => console.log(index)}
-				mode={MenuMode.Horizontal}
-			>
-				<MenuItem>link 1</MenuItem>
-				<MenuItem>link 2</MenuItem>
-				<SubMenu title="Submenu">
-					<MenuItem>link 1</MenuItem>
-					<MenuItem>link 2</MenuItem>
-				</SubMenu>
-				<MenuItem>link 3</MenuItem>
-			</Menu>
-
-			<hr />
-
-			<Menu
-				onSelect={index => console.log(index)}
-				mode={MenuMode.Vertical}
-				defaultExpandedVerticalSubMenus={['2']}
-			>
-				<MenuItem>link 1</MenuItem>
-				<MenuItem>link 2</MenuItem>
-				<SubMenu title="Submenu">
-					<MenuItem>link 1</MenuItem>
-					<MenuItem>link 2</MenuItem>
-				</SubMenu>
-				<MenuItem>link 3</MenuItem>
-			</Menu>
+			<Icon icon="coffee" theme="primary" size="10x" />
 		</div>
 	);
 }
