d91319fee099b19b3d616fcf8f6312a4ca40de16
Dennis Xiao <dennis.xiao@autogeneral.com.au>
2020-05-22 20:04:48 +0800

    fix: fix Icon component issue missing fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// Add fontawesome library
+library.add(fas);
+
 export { default as Button } from './components/Button';
 export { default as Menu } from './components/Menu';
 export { default as Input } from './components/Input';
